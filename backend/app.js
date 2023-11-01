fb1f63c4e58d34f6d29a2652d6b70b215801b650
reaganhsmith <reagansmith@byui.edu>
2023-11-01 10:12:18 -0600

    adding files

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
 const professionalRoutes = require('./routes/professional');
 
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(bodyParser.json());
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/professional', professionalRoutes);
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
- 
-app.use('/professional', professionalRoutes);
- 
-app.listen(process.env.PORT || 8080, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 8080));
-});
+
+
+  
+  
+    
+    
+  
+  
+
+
+  
--- backend/controllers/professional.js
+++ backend/controllers/professional.js
@@ -1,27 +1,11 @@
-const data = {
-    professionalName: 'Reagan Houser-Smith',
-    nameLink: {
-      firstName: 'Reagan',
-      url: 'https://nathanbirch.netlify.app/',
-    },
-    base64Image: '',
-    firstName: 'Reagan',
-    primaryDescription: 'is a student at BYU-Idaho',
-    workDescription1: 'She is a full-stack web developer',
-    workDescription2: 'She is a teachers assistant for WDD230',
-    linkTitleText: 'Check out her work below',
-    linkedInLink: {
-      link: 'https://www.linkedin.com/in/reagan-houser/',
-      text: 'LinkedIn'
-    },
-    githubLink: {
-      link: 'https://github.com/reaganhsmith',
-      text: 'GitHub'
-    },
-    contactText: 'Feel free to contact herZ at reagansmith@byui.edu if youd like for him to work for you!'
-}
+const mongodb = require('../db/connect');
 
-
-exports.getData = (req, res) =>{
-    res.status(200).json(data);
+const getData = async (req, res, next) => {
+  const result = await mongodb.getDb().db().collection('user').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]); // we just need the first one (the only one)
+  });
 };
+
+module.exports = { getData };
